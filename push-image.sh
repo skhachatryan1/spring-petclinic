2e5404903803f81d9a16734969626a2eba75563c
skhachatryan <skhachatryan@griddynamics.com>
2026-04-21 15:02:34 +0400

    scrips updated

--- push-image.sh
+++ push-image.sh
@@ -2,7 +2,7 @@
 
 gcloud auth configure-docker us-central1-docker.pkg.dev
 
-docker build -t petclinic-app .
+docker --platform=linux/amd64 build -t petclinic-app .
 
 docker tag petclinic-app \
     us-central1-docker.pkg.dev/gd-gcp-internship-devops/petclinic-docker-test/petclinic-app:latest
